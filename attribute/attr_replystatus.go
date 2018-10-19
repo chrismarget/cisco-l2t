d7df357f77c751ef032e65f1c9c19c80c03425d2
chris marget <cmarget@mutualink.net>
2018-10-18 22:30:24 -0400

    Added TestValidateDuplex()

--- attribute/attr_replystatus.go
+++ attribute/attr_replystatus.go
@@ -7,10 +7,61 @@ import (
 	"strings"
 )
 
+//Some stuff I found by strings-ing a binary:
+//
+//Multiple devices connected to %s port of %s [%i].
+//Layer2 trace aborted.
+//Invalid src/dest received from %s [%i].
+//Layer2 trace aborted.
+//Unable to locate port for src %e on %s [%i].
+//Layer2 trace aborted.
+//Unable to locate port for dst %e on %s [%i].
+//Layer2 trace aborted.
+//%e belongs to mutliple vlans on %s [%i].
+//Layer2 trace aborted.
+//Source and destination vlan mismatch discovered on %s [%i].
+//Layer2 trace aborted.
+//Failed to get any layer2 path information from %s [%i].
+//Layer2 trace aborted.
+//Layer2 path not through %s [%i].
+//Layer2 trace aborted.
+//Unknown return code %d from %s [%i].
+//
+//Invalid mac address
+//Invalid ip address
+//Mac found on multiple vlans
+//Source and destination macs are on different vlans
+//Device has multiple CDP neighbours
+//CDP neighbour has no ip
+//No CDP neighbour
+//Source Mac address not found
+//Destination Mac address not found
+//Incorrect source interface specified
+//Incorrect destination interface specified
+//Device has Multiple CDP neighbours on source port
+//Device has Multiple CDP neighbours on destination port
+
 const (
-	replyStatusSuccess       = "Success"
-	replyStatusNoCDPNeighbor = "No CDP Neighbor"
-	replyStatusUnknown       = "Status Unknown"
+	replyStatusSuccess = "Success"
+	//	replyStatusDstNotFound = "Destination Mac address not found"
+	//	replyStatusSrcNotFound = "Source Mac address not found"
+	replyStatusUnknown = "Status Unknown"
+
+	// The following strings were found together by strings-ing an IOS image.
+	// Leap of faith makes me think they're reply status attribute messages.
+	replyStatusInvalidMac           = "Invalid mac address"
+	replyStatusInvalidIP            = "Invalid ip address"
+	replyStausMultipleVlan          = "Mac found on multiple vlans"
+	replyStatusDifferentVlan        = "Source and destination macs are on different vlans"
+	replyStatusMultipleNeighbors    = "Device has multiple CDP neighbours"
+	replyStatusNoNeighborIP         = "CDP neighbour has no ip"
+	replyStatusNoNeighbor           = "No CDP neighbour"
+	replyStatusSrcNotFound          = "Source Mac address not found"      // l2t attr type 0x0f data 0x07
+	replyStatusDstNotFound          = "Destination Mac address not found" // l2t attr type 0x0f data 0x08
+	replyStatusWrongSrcInterface    = "Incorrect source interface specified"
+	replyStatusWrongDstInterface    = "Incorrect destination interface specified"
+	replyStatusSrcMultipleNeighbors = "Device has Multiple CDP neighbours on source port"
+	replyStatusDstMultipleNeighbors = "Device has Multiple CDP neighbours on destination port"
 )
 
 type (
@@ -20,7 +71,8 @@ type (
 var (
 	replyStatusToString = map[replyStatus]string{
 		1: replyStatusSuccess,
-		9: replyStatusNoCDPNeighbor,
+		7: replyStatusSrcNotFound,
+		8: replyStatusDstNotFound,
 	}
 )
 
@@ -64,13 +116,13 @@ func newReplyStatusAttr(t attrType, p attrPayload) (Attr, error) {
 		result.AttrData = []byte{byte(p.intData)}
 		return result, nil
 	}
-	return Attr{}, errors.New("Error creating reply status, no appropriate data supplied.")
+	return Attr{}, errors.New("Error creating reply status attribute, no appropriate data supplied.")
 }
 
 // validateReplyStatus checks the AttrType and AttrData against norms for
 // ReplyStatus type attributes.
 func validateReplyStatus(a Attr) error {
-	if attrCategoryByType[a.AttrType] != replyStatusCategory{
+	if attrCategoryByType[a.AttrType] != replyStatusCategory {
 		msg := fmt.Sprintf("Attribute type %d cannot be validated against reply status criteria.", a.AttrType)
 		return errors.New(msg)
 	}
