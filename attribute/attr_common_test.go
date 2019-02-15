475fd310ab01b1d9d5c11d317adee763aac09741
chris marget <cmarget@mutualink.net>
2019-02-15 07:54:15 -0500

    go fmt

--- attribute/attr_common_test.go
+++ attribute/attr_common_test.go
@@ -20,16 +20,16 @@ func TestUnMarshalAttribute_BadData(t *testing.T) {
 	var testData [][]byte
 	testData = append(testData, []byte{})
 	for i := 0; i <= math.MaxUint8; i++ {
-		testData[0] = append(testData[0], 1)			// oversize (fill 1st instance to 256 bytes)
+		testData[0] = append(testData[0], 1) // oversize (fill 1st instance to 256 bytes)
 	}
-	testData = append(testData, []byte{})						// undersize
-	testData = append(testData, []byte{1})						// undersize
-	testData = append(testData, []byte{1, 2})					// undersize
-	testData = append(testData, []byte{1, 8, 0, 0, 0, 0, 0})	// wrong length
+	testData = append(testData, []byte{})                    // undersize
+	testData = append(testData, []byte{1})                   // undersize
+	testData = append(testData, []byte{1, 2})                // undersize
+	testData = append(testData, []byte{1, 8, 0, 0, 0, 0, 0}) // wrong length
 
 	for t := 0; t <= math.MaxUint8; t++ {
 		if _, ok := attrCategoryByType[AttrType(t)]; !ok {
-			testData = append(testData, []byte{byte(t), 3, 1})	// fill testData with bogus types
+			testData = append(testData, []byte{byte(t), 3, 1}) // fill testData with bogus types
 		}
 	}
 
