d59ee249a28e39be1366ef5c8a223a81e58e3dc8
chris marget <cmarget@mutualink.net>
2019-08-06 13:58:55 -0400

    retries, backoff interval in each send routine

--- target/target.go
+++ target/target.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"github.com/chrismarget/cisco-l2t/attribute"
 	"github.com/chrismarget/cisco-l2t/message"
-	"log"
 	"net"
 	"strconv"
 	"time"
@@ -14,10 +13,9 @@ import (
 const (
 	udpPort           = 2228
 	UdpProtocol       = "udp4"
-	IPv4              = "ipv4"
 	nilIP             = "<nil>"
 	inBufferSize      = 65535
-	initialRTTGuess   = 17 * time.Millisecond
+	initialRTTGuess   = 50 * time.Millisecond
 	maxLatencySamples = 10
 	maxRetries        = 10
 	maxRTT            = 2500 * time.Millisecond
@@ -115,9 +113,15 @@ func (o *defaultTarget) String() string {
 	return out.String()
 }
 
-//TODO timeout, retry, etc
+// communicateViaConventionalSocket sends a byte slice to the target using a UDP
+// socket. It includes retry logic for handling packet loss. On success it
+// returns a byte slice containing the reply payload.
+//
+// This function is useful for targets that reply from an address other
+// than the one we talk to. In an ideal world only the communicateViaDialSocket
+// method would be required, but some firewall/NAT situations may force the use
+// of this method.
 func (o *defaultTarget) communicateViaConventionalSocket(b []byte) ([]byte, error) {
-	var rtt time.Duration
 	destination := &net.UDPAddr{
 		IP:   o.theirIp[o.talkToThemIdx],
 		Port: udpPort,
@@ -129,20 +133,99 @@ func (o *defaultTarget) communicateViaConventionalSocket(b []byte) ([]byte, erro
 	}
 	defer cxn.Close()
 
-
-	// todo: there's no retry, no timeout here yet
 	buffIn := make([]byte, inBufferSize)
 
+	var rtt time.Duration
 	received := 0
-	retries := 0
+	attempts := 0
 	for received == 0 {
-		if retries >= maxRetries {
-			return nil, fmt.Errorf("Lost connection with switch %s after %d retries", destination.IP.String(), retries)
+		if attempts >= maxRetries {
+			return nil, fmt.Errorf("lost connection with switch %s after %d attempts", destination.IP.String(), attempts)
 		}
 		wait := o.estimateLatency()
 
-		// Send the packet
+		// Send the packet. Error handling happens after noting the start time.
 		n, err := cxn.WriteToUDP(b, destination)
+
+		// collect start time for later RTT calculation
+		start := time.Now()
+
+		switch {
+		case err != nil:
+			return nil, err
+		case n != len(b):
+			return nil, fmt.Errorf("attemtped send of %d bytes, only managed %d", len(b), n)
+		}
+
+		// set deadline based on start time
+		err = cxn.SetReadDeadline(start.Add(wait))
+		if err != nil {
+			return nil, err
+		}
+
+		// read until packet or deadline
+		var respondent *net.UDPAddr
+		received, respondent, err = cxn.ReadFromUDP(buffIn)
+
+		// Note the elapsed time
+		rtt = time.Since(start)
+
+		// How might things have gone wrong?
+		switch {
+		case err != nil:
+			if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+				// Socket timeout; Double the timeout interval, stick it in the latency history.
+				attempts += 1
+				o.updateLatency(2 * rtt)
+				continue
+			} else {
+				// Mystery error
+				return nil, fmt.Errorf("error waiting for reply via conventional socket - %s", err.Error())
+			}
+		case n == len(buffIn):
+			// Unexpectedly large read
+			return nil, fmt.Errorf("got full buffer: %d bytes", n)
+		case !respondent.IP.Equal(o.theirIp[o.listenToThemIdx]):
+			// Alien reply. Nudge the latency budget, try again.
+			o.updateLatency(wait)
+			continue
+		}
+	}
+	o.updateLatency(rtt)
+	return buffIn, nil
+}
+
+// communicateViaDialSocket sends a byte slice to the target using a UDP
+// socket. It includes retry logic for handling packet loss. On success it
+// returns a byte slice containing the reply payload.
+//
+// Because this function uses the net.DialUDP method it is only useful for
+// targets that reply from the address to which we sent the query. That is the
+// natural behavior of most UDP services, but not the Cisco L2T server. This is
+// the preferred method because it's friendly to stateful middleboxes like NAT.
+func (o defaultTarget) communicateViaDialSocket(b []byte) ([]byte, error) {
+	destination := &net.UDPAddr{
+		IP:   o.theirIp[o.talkToThemIdx],
+		Port: udpPort,
+	}
+
+	cxn, err := net.DialUDP(UdpProtocol, &net.UDPAddr{}, destination)
+	if err != nil {
+		return nil, err
+	}
+	defer cxn.Close()
+
+	buffIn := make([]byte, inBufferSize)
+
+	var rtt time.Duration
+	received := 0
+	attempts := 0
+	for received == 0 {
+		if attempts >= maxRetries {
+			return nil, fmt.Errorf("lost connection with switch %s after %d attempts", destination.IP.String(), attempts)
+		}
+		wait := o.estimateLatency()
+		n, err := cxn.Write(b)
 		switch {
 		case err != nil:
 			return nil, err
@@ -158,15 +241,15 @@ func (o *defaultTarget) communicateViaConventionalSocket(b []byte) ([]byte, erro
 		}
 
 		// read until packet or deadline
-		var respondent *net.UDPAddr
-		received, respondent, err = cxn.ReadFromUDP(buffIn)
+		received, err = cxn.Read(buffIn)
 		rtt = time.Since(start)
-		// How can things go wrong here?
+
+		// How might things have gone wrong?
 		switch {
 		case err != nil:
 			if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
 				// Socket timeout; Double the timeout interval, stick it in the latency history.
-				retries += 1
+				attempts += 1
 				o.updateLatency(2 * rtt)
 				continue
 			} else {
@@ -176,42 +259,9 @@ func (o *defaultTarget) communicateViaConventionalSocket(b []byte) ([]byte, erro
 		case n == len(buffIn):
 			// Unexpectedly large read
 			return nil, fmt.Errorf("got full buffer: %d bytes", n)
-		case !respondent.IP.Equal(o.theirIp[o.listenToThemIdx]):
-			// Alien reply
-			tIp := o.theirIp[o.talkToThemIdx].String()
-			eIp := o.theirIp[o.listenToThemIdx].String()
-			aIp := respondent.IP.String()
-			return nil, fmt.Errorf("%s replied from unexpected address %s, rather than %s", tIp, aIp, eIp)
 		}
 	}
-	o.updateLatency(rtt)
-	return buffIn, nil
-}
 
-// todo timeout, etc..
-func (o defaultTarget) communicateViaDialSocket(b []byte) ([]byte, error) {
-	destination := &net.UDPAddr{
-		IP:   o.theirIp[o.talkToThemIdx],
-		Port: udpPort,
-	}
-
-	cxn, err := net.DialUDP(UdpProtocol, &net.UDPAddr{}, destination)
-	if err != nil {
-		return nil, err
-	}
-	defer cxn.Close()
-
-	buffIn := make([]byte, inBufferSize)
-	//	start := time.Now()
-	n, err := cxn.Write(b)
-	switch {
-	case err != nil:
-		return nil, err
-	case n != len(b):
-		return nil, fmt.Errorf("attemtped send of %d bytes, only managed %d", len(b), n)
-	}
-
-	cxn.Read(buffIn)
 	return buffIn, nil
 }
 
@@ -236,14 +286,10 @@ func (o *defaultTarget) estimateLatency() time.Duration {
 // updateLatency adds the passed time.Duration as the most recent
 // latency sample, trims the latency slice to size.
 func (o *defaultTarget) updateLatency(t time.Duration) {
-	log.Println("samples: ",o.latency)
-	log.Println("update:", t)
 	o.latency = append(o.latency, t)
-	log.Println("new samples: ",o.latency)
 	// delete old elements of latency slice because we care more about
 	// recent data (and certainly want to purge early bad assumptions)
 	if len(o.latency) > maxLatencySamples {
-		log.Println("trim")
 		o.latency = o.latency[len(o.latency)-maxLatencySamples : len(o.latency)]
 	}
 
