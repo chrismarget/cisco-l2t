abec168b85e861eef0bd97f91965305e4420d19b
Chris Marget <5470536+chrismarget@users.noreply.github.com>
2019-08-14 14:56:11 -0400

    builder uses rudp.Communicate()

--- target/builder_test.go
+++ target/builder_test.go
@@ -1,1 +1,14 @@
 package target
+
+import (
+	"net"
+	"testing"
+)
+
+func TestCheckTargetIP(t *testing.T) {
+	ip := net.ParseIP("192.168.15.254")
+	result := checkTargetIp(ip)
+	if result.err != nil {
+		t.Fatal(result.err)
+	}
+}
