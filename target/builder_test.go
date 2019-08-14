e1b9815e1b72f4732606eea32badfcbad2463342
chris marget <cmarget@mutualink.net>
2019-08-14 16:31:37 -0400

    checkTargetIP uses both socket types

--- target/builder_test.go
+++ target/builder_test.go
@@ -1,14 +1,16 @@
 package target
 
 import (
+	"log"
 	"net"
 	"testing"
 )
 
 func TestCheckTargetIP(t *testing.T) {
-	ip := net.ParseIP("192.168.15.254")
+	ip := net.ParseIP("192.168.8.254")
 	result := checkTargetIp(ip)
 	if result.err != nil {
 		t.Fatal(result.err)
 	}
+	log.Println("reply from:",result.IP)
 }
