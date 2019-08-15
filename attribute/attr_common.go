1175397e01bbe4f23d9d2920bcda78a7a84637d8
chris marget <cmarget@mutualink.net>
2019-08-15 18:03:22 -0400

    added SortAttributes()

--- attribute/attr_common.go
+++ attribute/attr_common.go
@@ -361,3 +361,15 @@ func AttrStringToType(in string) (AttrType, error) {
 	return 0, fmt.Errorf("unknown attribute type %s", in)
 }
 
+// SortAttributes sorts a map (what you'd get from message.Attributes())
+// into a slice with the attributes in numerical order.
+func SortAttributes(in map[AttrType]Attribute) []Attribute {
+	var i AttrType
+	var out []Attribute
+	for i = 0; int(i) <= math.MaxUint8; i++ {
+		if a, ok := in[i]; ok {
+			out = append(out, a)
+		}
+	}
+	return out
+}
