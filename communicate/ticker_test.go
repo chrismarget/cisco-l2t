05eb7c5e6642abff8b04b353e3b706efc1aba4f5
Chris Marget <5470536+chrismarget@users.noreply.github.com>
2019-08-22 12:36:40 -0400

    TestBackoffTicker is doing some math now

--- communicate/ticker_test.go
+++ communicate/ticker_test.go
@@ -1,24 +1,49 @@
 package communicate
 
 import (
-	"log"
 	"testing"
 	"time"
 )
 
-// todo do some math here, check it out
 func TestBackoffTicker(t *testing.T) {
-	start := time.Now()
-	end := start.Add(5*time.Second)
-	bot := NewBackoffTicker(100*time.Millisecond)
-	log.Println("start:",start)
-	for time.Now().Before(end){
-		select {
-		case tick := <- bot.C:
-			log.Println("tick:", tick)
-		}
+	type testData struct {
+		ticks           int
+		initialInterval time.Duration
+		estimate        time.Duration
 	}
-	bot.Stop()
-	time.Sleep(10*time.Second)
-}
 
+	var testDataSet []testData
+	testDataSet = append(testDataSet,
+		testData{
+			ticks:           5,
+			initialInterval: 100 * time.Millisecond,
+			estimate:        800 * time.Millisecond,
+		})
+	testDataSet = append(testDataSet,
+		testData{
+			ticks:           8,
+			initialInterval: 25 * time.Millisecond,
+			estimate:        1600 * time.Millisecond,
+		})
+
+	for _, test := range testDataSet {
+		start := time.Now()
+		bot := NewBackoffTicker(test.initialInterval)
+		threshold := test.estimate + (test.estimate / 20)
+		end := start.Add(threshold)
+
+		tickCount := 0
+		for time.Now().Before(end) && tickCount < test.ticks {
+			<-bot.C
+			tickCount++
+		}
+		elapsed := time.Now().Sub(start)
+		if elapsed >= threshold {
+			t.Fatalf("test ran long: limit %s, elapsed %s", threshold, elapsed)
+		}
+		if elapsed < test.estimate {
+			t.Fatalf("test ran short : limit %s, elapsed %s", test.estimate, elapsed)
+		}
+		bot.Stop()
+	}
+}
